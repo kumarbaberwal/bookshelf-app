7e0fd39e878863051bebeca7efdb349a063a0cb3
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-03-17 13:45:09 +0530

    add new books, get all books with pagination

--- backend/src/controllers/bookControllers.ts
+++ backend/src/controllers/bookControllers.ts
@@ -13,12 +13,15 @@ export const bookController = async (req: Request, res: Response): Promise<any> 
         }
 
         // upload the image to cloudinary
-        // save this to the mongodb
 
         const uploadResponse = await cloudinary.uploader.upload(image);
+
+        // get the image url from the cloudinary
+
         const imageUrl = uploadResponse.secure_url;
 
 
+        // save this to the mongodb
         const newBook = new Book({
             title: title,
             caption: caption,
@@ -35,5 +38,36 @@ export const bookController = async (req: Request, res: Response): Promise<any> 
         res.status(500).json({
             message: "Error creating book",
         })
+    }
+}
+
+// pagination => infinite scrolling
+// first 5 books => then 5 books => then 5 books
+const getAllBooks = async (req: Request, res: Response): Promise<any> => {
+    try {
+        const page = parseInt(req.query.page as string, 10) || 1;
+        const limit = parseInt(req.query.limit as string, 10) || 5;
+        const skip = (page - 1) * limit;
+        const books = await Book.find()
+            .sort({ createdAt: -1 })  // Sort books in descending order by creation date
+            .skip(skip)               // Skip over a number of documents (for pagination)
+            .limit(limit)             // Limit the number of documents returned
+            .populate('user', 'username profileImage');  // Populate 'user' field with specific user fields ('username', 'profileImage')
+
+        const totalBooks = await Book.countDocuments();
+
+        res.status(200).json({
+            books,
+            currentPage: page,
+            totalBooks,
+            totalPages: Math.ceil(totalBooks / limit),
+        })
+
+
+    } catch (error) {
+        console.log("Error in Fetching All Books: ", error);
+        res.status(500).json({
+            message: 'Error in Fetching All Books',
+        });
     }
 }
