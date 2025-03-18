160e297a61440fe751ee8c7e85bb99918814fb21
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-03-19 00:00:14 +0530

    mv types/index.d.ts types/express.d.ts

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -3,15 +3,14 @@ import Jwt from "jsonwebtoken";
 import { ENV_VARS } from "../configs/config";
 
 export const veryfyToken = (req: Request, res: Response, next: NextFunction): void => {
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token) {
-        res.status(403).json({
-            message: "No authenticated token provided, access denied"
-        });
-        return;
-    }
-
     try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            res.status(403).json({
+                message: "No authenticated token provided, access denied"
+            });
+            return;
+        }
         const decode = Jwt.verify(token, ENV_VARS.JWT_SECRET) as { userId: string };
         req.user = decode;
         next();
